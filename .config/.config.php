2b9db0cd6b89f3b4f7f1b33fbc5d866c1b5b2396
ari <ari@asu.edu>
2015-02-28 12:58:52 -0700

    config fix

--- .config/.config.php
+++ .config/.config.php
@@ -1,10 +1,9 @@
 <?php
-
-\Site\PGP\PGPConfig::$GPGPath = '/usr/bin/gpg';
-
 error_reporting(E_ALL & ~E_NOTICE);
+\CPath\Build\BuildConfig::$BUILD_FILES = true;
 \Site\Config::$RequireInvite = false;
-
 \Site\Config::$ChatSocketURI = 'ws://snesology.com:7845/';
-
-// \CPath\Build\BuildConfig::$BUILD_FILES = true;
+\Site\PGP\PGPConfig::$GPGPath = '/usr/bin/gpg';
+//\Site\PGP\PGPConfig::$GPGPath = 'C:/cygwin/bin/gpg.exe';
+//\Site\DB\DBConfig::$DB_NAME = 'ari_snesology';
+//\Site\DB\DBConfig::$DB_PASSWORD = 'password';
